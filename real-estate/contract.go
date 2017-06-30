be014b237b6540412cd3fe6d56ffac75137624f7
crane <kcrane@ippon.fr>
2017-06-30 18:46:05 -0400

    added getHistoryForProperty

--- real-estate/contract.go
+++ real-estate/contract.go
@@ -14,14 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-//TODO
-// - getOwnership(id) Ownership (CurrentState)
-// - getAllOwnership(list<id>) list<Ownership>
-// - getOwnershipHistory(id) Ownership (History)
-// - getProperty(id) Property (CurrentState)
-// - getAllProperty(list<id>) list<Property> (CurrentState)
-// - getPropertyHistory(id) Property (History)
-// - propertyTransaction(Date, SalePrice, list<Attribute>) nil
 package main
 
 import (
@@ -32,6 +24,7 @@ import (
 	"encoding/json"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
+	"bytes"
 )
 
 type Chaincode struct {
@@ -44,6 +37,8 @@ type Chaincode struct {
 // - getPropertyHistory(id) Property (History)
 // - propertyTransaction(Date, SalePrice, list<Attribute>) nil
 
+//TODO
+// - make sure errors are all handled (custom responses where needed)
 type Ownership struct {
 	Properties	[]Attribute			`json:"properties"`
 }
@@ -64,62 +59,6 @@ func (t *Chaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	return shim.Success(nil)
 }
 
-
-func (t *Chaincode) propertySale(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var propertyId string
-	var propertyString string
-	var err error
-
-	if len(args) != 2 {
-		return shim.Error("Incorrect number of arguments. Expecting 4")
-	}
-
-	propertyId = args[0]
-	propertyString = args[1]
-	fmt.Printf("PropertyId = %d", propertyId)
-
-	property := Property{}
-
-	err = json.Unmarshal([]byte(propertyString), &property)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = confirmValidPercentage(property.Owners)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	// Write the state to the ledger
-	propertyAsBytes, err := getPropertyAsBytes(property)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(propertyId, propertyAsBytes)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	return shim.Success(nil)
-}
-
-func confirmValidPercentage(buyers []Attribute) error{
-	var totalPercentage float64
-	var err error
-
-	for i := 0; i < len(buyers); i++ {
-		totalPercentage += buyers[i].Percentage
-	}
-
-	if totalPercentage != 1 {
-		totalPercentageString := fmt.Sprint(totalPercentage)
-		err = errors.New("Total Percentage is not correct: " + totalPercentageString)
-	}
-
-	return err
-}
-
 func (t *Chaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	fmt.Printf("Invoke")
 	function, args := stub.GetFunctionAndParameters()
@@ -132,12 +71,18 @@ func (t *Chaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	} else if function == "query" {
 		// the old "Query" is now implemtned in invoke
 		return t.query(stub, args)
-	} else if function == "findAll" {
+	} else if function == "queryProperty" {
+		// the old "Query" is now implemtned in invoke
+		return t.queryProperty(stub, args)
+	}else if function == "findAll" {
 		return t.findAll(stub)
 	} else if  function == "propertySale" {
 		return t.propertySale(stub, args)
+	} else if function == "getHistoryForProperty" {
+		return t.getHistoryForProperty(stub, args)
 	}
 
+	//TODO update
 	return shim.Error("Invalid invoke function name. Expecting \"move\" \"delete\" \"query\" \"findAll\"")
 }
 
@@ -229,8 +174,7 @@ func (t *Chaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Re
 	// Get the state from the ledger
 	Avalbytes, err := stub.GetState(A)
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-		return shim.Error(jsonResp)
+		return shim.Error(err.Error())
 	}
 
 	if Avalbytes == nil {
@@ -371,6 +315,155 @@ func getFormattedTimeAsString(time time.Time, format string)(string, error){
 
 	return formattedTimeString, err
 
+}
+
+func confirmValidPercentage(buyers []Attribute) error{
+	var totalPercentage float64
+	var err error
+
+	for i := 0; i < len(buyers); i++ {
+		totalPercentage += buyers[i].Percentage
+	}
+
+	if totalPercentage != 1 {
+		totalPercentageString := fmt.Sprint(totalPercentage)
+		err = errors.New("Total Percentage is not correct: " + totalPercentageString)
+	}
+
+	return err
+}
+
+//peer chaincode query -C mychannel -n mycc -c '{"Args":["getHistoryForProperty","property_1"]}'
+func (t *Chaincode) getHistoryForProperty(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	propertyId := args[0]
+
+	resultsIterator, err := stub.GetHistoryForKey(propertyId)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing historic values for the marble
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		response, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"TxId\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(response.TxId)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Value\":")
+		// if it was a delete operation on given key, then we need to set the
+		//corresponding value null. Else, we will write the response.Value
+		//as-is (as the Value itself a JSON property)
+		if response.IsDelete {
+			buffer.WriteString("null")
+		} else {
+			buffer.WriteString(string(response.Value))
+		}
+
+		//buffer.WriteString(", \"Timestamp\":")
+		//buffer.WriteString("\"")
+		//buffer.WriteString(time.Unix(response.Timestamp.Seconds, int64(response.Timestamp.Nanos)).String())
+		//buffer.WriteString("\"")
+		//
+		//buffer.WriteString(", \"IsDelete\":")
+		//buffer.WriteString("\"")
+		//buffer.WriteString(strconv.FormatBool(response.IsDelete))
+		//buffer.WriteString("\"")
+
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("- getHistoryForProperty returning:\n%s\n", buffer.String())
+
+	return shim.Success(buffer.Bytes())
+}
+
+//peer chaincode invoke -C mychannel -n mycc -c '{"Args":["propertySale","property_1","{\"saleDate\": \"2017-06-28T21:57:16\", \"salePrice\": 1000, \"owners\": [{\"id\":\"owner_3\",\"percentage\":0.45},{\"id\":\"owner_2\",\"percentage\":0.55}]}"]}'
+//peer chaincode invoke -C mychannel -n mycc -c '{"Args":["propertySale","property_1","{\"saleDate\": \"2017-06-28T21:57:16\", \"salePrice\": 1000, \"owners\": [{\"id\":\"owner_1\",\"percentage\":0.32},{\"id\":\"owner_4\",\"percentage\":0.68}]}"]}'
+func (t *Chaincode) propertySale(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var propertyId string
+	var propertyString string
+	var err error
+
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
+	}
+
+	propertyId = args[0]
+	propertyString = args[1]
+	fmt.Printf("PropertyId = %d", propertyId)
+
+	property := Property{}
+
+	err = json.Unmarshal([]byte(propertyString), &property)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	err = confirmValidPercentage(property.Owners)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Write the state to the ledger
+	propertyAsBytes, err := getPropertyAsBytes(property)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	err = stub.PutState(propertyId, propertyAsBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
+}
+
+//peer chaincode query -C mychannel -n mycc -c '{"Args":["queryProperty","property_1"]}'
+func (t *Chaincode) queryProperty(stub shim.ChaincodeStubInterface, args []string) pb.Response{
+	var propertyId string
+	var err error
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting property id to query")
+	}
+
+	propertyId = args[0]
+
+	// Get the state from the ledger
+	propertyBytes, err := stub.GetState(propertyId)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	if propertyBytes == nil {
+		jsonResp := "{\"Error\":\"Nil amount for " + propertyId + "\"}"
+		return shim.Error(jsonResp)
+	}
+
+	jsonResp := "{\"PropertyId\":\"" + propertyId + "\",\"Property Struct\":\"" + string(propertyBytes) + "\"}"
+	fmt.Printf("Query Response:%s\n", jsonResp)
+
+	return shim.Success(propertyBytes)
 }
 
 func main() {
