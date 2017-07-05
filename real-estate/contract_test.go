cb838ec282413c4de5c8072ecfd3822e47b3c8ae
crane <kcrane@ippon.fr>
2017-07-05 12:40:14 -0400

    added tests for creating propertyTransaction, checking that property is in the ledger, and querying for property in the ledger

--- real-estate/contract_test.go
+++ real-estate/contract_test.go
@@ -20,6 +20,8 @@ import (
 	"fmt"
 	"testing"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"strconv"
+	"bytes"
 )
 
 type TestAttribute struct {
@@ -47,6 +49,35 @@ func TestGetOwnership(t *testing.T){
 	checkInvokeOwnership(t, stub, ownership)
 	checkGetOwnership(t, stub, ownership)
 
+}
+
+func TestPropertyTransaction(t *testing.T){
+
+	stub := getStub()
+	owner1 := `"id":"ownership_3","percentage":0.45`
+	owner2 := `"id":"ownership_2","percentage":0.55`
+
+	owners := []string{owner1, owner2}
+
+	property := getProperty("property_1", `"2017-06-28T21:57:16"`, 1000, owners)
+
+	checkPropertyTransaction(t,stub, property)
+	checkPropertyState(t, stub, property)
+
+}
+
+func TestGetProperty(t *testing.T){
+
+	stub := getStub()
+	owner1 := `"id":"ownership_3","percentage":0.45`
+	owner2 := `"id":"ownership_2","percentage":0.55`
+
+	owners := []string{owner1, owner2}
+
+	property := getProperty("property_1", `"2017-06-28T21:57:16"`, 1000, owners)
+
+	checkPropertyTransaction(t, stub, property)
+	checkGetProperty(t, stub, property)
 }
 
 
@@ -100,11 +131,117 @@ func checkGetOwnership(t *testing.T, stub *shim.MockStub, ownership TestAttribut
 
 }
 
+func checkPropertyTransaction(t *testing.T, stub *shim.MockStub, property TestAttribute){
+
+	function := "propertyTransaction"
+
+	propertyArgs := create(function, property.Key, property.Value)
+
+	res := stub.MockInvoke(function, propertyArgs)
+	if res.Status != shim.OK {
+		fmt.Println("InvokeProperty", property, "failed", string(res.Message))
+		t.FailNow()
+	}
+
+}
+
+func checkPropertyState(t *testing.T, stub *shim.MockStub, property TestAttribute) {
+
+	bytes := stub.State[property.Key]
+	if bytes == nil {
+		fmt.Println("Property", property, "failed to get value")
+		t.FailNow()
+	}
+	if string(bytes) != property.Value {
+		fmt.Println("Property value", property.Key, "was not", property.Value, "as expected")
+		t.FailNow()
+	}
+
+}
+
+func checkGetProperty(t *testing.T, stub *shim.MockStub, property TestAttribute){
+
+	function := "getProperty"
+
+	propertyArgs := get(function, property.Key)
+
+	res := stub.MockInvoke(function, propertyArgs)
+	if res.Status != shim.OK {
+		fmt.Println(function, property, "failed", string(res.Message))
+		t.FailNow()
+	}
+	if res.Payload == nil {
+		fmt.Println(function, property, "failed to get value")
+		t.FailNow()
+	}
+	if string(res.Payload) != property.Value {
+		fmt.Println(function, " value", property.Key, "was not", property.Value, "as expected")
+		t.FailNow()
+	}
+
+}
+
 func getOwnership(ownershipId string, properties string) (TestAttribute){
 
 	ownership := TestAttribute{ownershipId, properties}
 
 	return ownership
+
+}
+
+func getProperty(propertyId string, saleDate string, salePrice float64, owners []string) (TestAttribute) {
+
+	var buffer bytes.Buffer
+
+	buffer.WriteString("{")
+
+	saleDateKey := `"saleDate":`
+	buffer.WriteString(saleDateKey)
+	buffer.WriteString(saleDate)
+	buffer.WriteString(",")
+
+	salePriceKey := `"salePrice":`
+	buffer.WriteString(salePriceKey)
+	jsonSalePrice := strconv.FormatFloat(salePrice, 'f', -1, 64)
+	buffer.WriteString(jsonSalePrice)
+	buffer.WriteString(",")
+
+	ownersKey := `"owners":`
+	buffer.WriteString(ownersKey)
+	ownersAttribute := getOwners(propertyId, owners)
+	buffer.WriteString(ownersAttribute.Value)
+
+	buffer.WriteString("}")
+
+	jsonProperty := TestAttribute{propertyId, buffer.String()}
+
+	return jsonProperty
+
+}
+
+func getOwners(propertyId string, owners []string) (TestAttribute) {
+
+	var buffer bytes.Buffer
+
+	buffer.WriteString("[")
+
+	for i := 0; i < len(owners); i++ {
+
+		buffer.WriteString("{")
+		buffer.WriteString(owners[i])
+		buffer.WriteString("}")
+
+		if i != (len(owners) - 1){
+			buffer.WriteString(",")
+		}
+
+	}
+
+	buffer.WriteString("]")
+
+	propertyOwners := TestAttribute{propertyId, buffer.String()}
+
+	return propertyOwners
 
 }
 
@@ -128,9 +265,6 @@ func get(function string, id string) ([][]byte) {
 
 	return args
 }
-
-
-
 
 func getStub() (*shim.MockStub){
 
